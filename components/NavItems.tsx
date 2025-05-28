fd5912b96e43c70f82d47647ed01e0e404b1b296
Bablu 202 <aware9758@gmail.com>
2025-05-28 09:57:24 +0200

    --

---
+++ components/NavItems.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavItems = () => {
+  return <div></div>;
+};
+
+export default NavItems;
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Companions", href: "/companions" },
+  { label: "M", href: "/M" },
+];
