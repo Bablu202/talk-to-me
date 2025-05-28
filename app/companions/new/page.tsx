39f5338dde3ae58c48dfa629ef5ab8ec43b8966a
Bablu 202 <aware9758@gmail.com>
2025-05-28 08:34:55 +0200

    --

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>new add companion</div>;
+};
+
+export default page;
---
+++ app/companions/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>companiions library</div>;
+};
+
+export default page;
